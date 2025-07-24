5864dec8001d9f23ef92479f7da2d64bae580d6f
dangerberg <danackermangreenberg@gmail.com>
2025-07-24 00:42:03 -0700

    Update metadata

--- app/guides/[slug]/page.tsx
+++ app/guides/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation"
+import type { Metadata } from "next"
 import { createSupabaseServerClient } from "@/lib/supabase/server"
 import GuidePageClient, { type GuideData } from "./guide-page-client"
 
@@ -7,6 +8,40 @@ import GuidePageClient, { type GuideData } from "./guide-page-client"
  * SUPABASE_SERVICE_ROLE_KEY & SUPABASE_URL.  No client-side code here!
  */
 export const revalidate = 0
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}): Promise<Metadata> {
+  const supabase = createSupabaseServerClient()
+  const { data } = await supabase
+    .from("guides")
+    .select("guide_data")
+    .eq("slug", params.slug)
+    .single()
+
+  if (!data) {
+    return {}
+  }
+
+  const guideData = data.guide_data as GuideData
+
+  const title = `${guideData.gameName} | Game Design Club`
+  const description = guideData.gameSubtitle
+
+  return {
+    title,
+    description,
+    openGraph: { images: "/IMG_6499.png", title, description },
+    twitter: {
+      card: "summary_large_image",
+      images: "/IMG_6499.png",
+      title,
+      description,
+    },
+  }
+}
 
 export default async function GuidePage({
   params,
